32741e578b2ef8490b22f8e49f02d0024165ba5a
aptmess <improfeo@yandex.ru>
2022-07-19 23:20:21 +0300

    add notebooks folder

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rbk
 rbk news popularity prediction solution
+
+# Venv
+
+- `make venv`
+- `python -m ipykernel install --name rbk --user`
---
+++ definitions.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parent
+
+DATA_DIR = ROOT_DIR / 'data'
+
+SUB_DIR = ROOT_DIR / 'sub'
+
+NOTEBOOKS_DIR = ROOT_DIR / 'notebooks'
+
+INPUT_DATA_DIR = DATA_DIR / 'input'
+
+PARSED_DATA_DIR = DATA_DIR / 'parsed'
+
+FULL_DATA_DIR = DATA_DIR / 'full'
